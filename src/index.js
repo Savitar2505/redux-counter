4daaa40eb7a0025ae9b308a82662a62a3655c620
Savitar2505 <us_vladislav@mail.ru>
2022-03-09 14:48:55 +0600

    done redux counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import store from "./store";
+import * as actions from './actions'
+import {bindActionCreators} from 'redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const countDisplay = document.getElementById('count-display')
+const incrementBtn = document.getElementById('increment')
+const decrementBtn = document.getElementById('decrement')
+
+const {inc, dec}=bindActionCreators(store.dispatch, actions)
+
+incrementBtn.addEventListener('click', inc)
+decrementBtn.addEventListener('click', dec)
+
+store.subscribe(()=>countDisplay.value =store.getState())
